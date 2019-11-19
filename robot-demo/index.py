16335826b14e3b4413fc680bac41e5d5555f248d
wayley <hb_wangzheng@163.com>
2019-11-19 10:28:54 +0800

    modified robot

--- robot-demo/index.py
+++ robot-demo/index.py
@@ -4,10 +4,9 @@ from wxpy import *
 import requests
 import random
 bot = Bot()
+my_friend = bot.friends().search('Omine')[0]
 
 
-# linux执行登陆请调用下面的这句
-#bot = Bot(console_qr=2,cache_path="botoo.pkl")
 def get_news():
     url = "http://open.iciba.com/dsapi/"
     r = requests.get(url)
@@ -16,25 +15,40 @@ def get_news():
     return content, note
 
 
+def greet():
+    contents = get_news()
+    my_friend.send(u"每日一句,小晶晶你准备好了吗?")
+    my_friend.send(contents[0])
+    my_friend.send(contents[1])
+
+
+sentence = [
+    '晶晶小姐姐，我错了', '我就是个坏人，我伤害了你', '您大人不记小人过', '我不敢求您原谅我', '但您别跟自己置气好不好',
+    '你是世界上最可爱的小仙女'
+]
+sentence_en = [
+    'Miss Jing, it`s all my fault', 'I am a bad man, and i hurt you',
+    'Your excellency does not remember villains', 'I dare not beg your pardon',
+    'But don`t get angry with yourself',
+    'You are the loveliest fairy in the world'
+]
+index = 0
+
+
 def send_news():
     try:
-        contents = get_news()
-        # 你朋友的微信名称，不是备注，也不是微信帐号。
-        my_friend = bot.friends().search('Omine')[0]
-        my_friend.send(contents[0])
-        my_friend.send(contents[1])
-        my_friend.send(u"我是机器人wz, 前面两句是我在金山词霸上面抓取的英文和翻译~~~~ 爱雪宝宝的呀")
-
-        ran_int = random.randint(0, 10)
-        t = Timer(10 + ran_int, send_news)
-
+        global index
+        if index > len(sentence_en) - 1:
+            index = 0
+        my_friend.send(sentence[index])
+        my_friend.send(sentence_en[index])
+        index = index + 1
+        t = Timer(600, send_news)
         t.start()
     except:
-
-        # 你的微信名称，不是微信帐号。
-        my_friend = bot.friends().search('Omine')[0]
-        my_friend.send(u"我是机器人wz,网络不好, -----爱雪宝宝的呀")
+        my_friend.send(u"我是机器人wz,网络不好, -----")
 
 
 if __name__ == "__main__":
+    greet()
     send_news()
